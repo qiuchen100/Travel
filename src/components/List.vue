a99494a656835becc0ba18ff3bf3600ff44a2ce7
qiuchen <qiuchen100@163.com>
2020-02-11 14:12:50 +0800

    修改并初始化项目

---
+++ src/components/List.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="MyList">{{message}}</div>
+</template>
+<script>
+export default {
+  name: "List",
+  data() {
+    return {
+      message: "List"
+    };
+  }
+};
+</script>
+<style>
+.MyList {
+  font-size: 50px;
+}
+</style>
